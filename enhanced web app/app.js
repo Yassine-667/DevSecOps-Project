a34a157360557554a70ab4711f58f8999127b725
Yassine Lazrak <lazrak20016@gmail.com>
2024-04-22 04:56:53 +0000

    added logo to the web app , as well as some other minor modifications

--- enhanced web app/app.js
+++ enhanced web app/app.js
@@ -13,7 +13,9 @@ if (!fs.existsSync(UPLOAD_FOLDER)) {
 }
 
 app.use(fileUpload());
+
 app.use(express.static(path.join(__dirname, 'static')));
+app.use('/static', express.static(path.join(__dirname, 'static')));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'templates', 'index.html'));
